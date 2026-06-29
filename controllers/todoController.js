994b307f39d81a920a4d0ffaa178298c7756458b
Keyur <keyur.shah@confiancebizsol.com>
2026-06-29 14:34:31 +0530

    Initial commit - Task management system relative url update

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -33,26 +33,7 @@ const getTodoById = async (req, res, next) => {
 };
 
 
-// const createTodo = async (req, res) => {
-//   const { title, description, completed, priority, dueDate } = req.body;
 
-//   if (!title) {
-//     return res.status(400).json({ message: "Title are Required" });
-//   }
-
-//   const newTodo = new Todo({
-//     title,
-//     description,
-//     completed,
-//     priority,
-//     dueDate,
-//     owner: req.user.userId,
-//   });
-
-//   await newTodo.save();
-
-//   res.status(201).json(newTodo);
-// };
 
 const createTodo = async (req, res, next) => {
   try {
@@ -82,35 +63,7 @@ const createTodo = async (req, res, next) => {
   }
 };
 
-// const updateTodo = async (req, res) => {
-//   const { title, description, completed, priority, dueDate } = req.body;
 
-//   if (!title) {
-//     return res.status(400).json({ message: "Title are Required" });
-//   }
-
-//   const updatedTodo = await Todo.findOneAndUpdate(
-//     {
-//       _id: req.params.id,
-//       owner: req.user.userId,
-//     },
-//     {
-//       title,
-//       description,
-//       completed,
-//       priority,
-//       dueDate,
-//     },
-//     {
-//       new: true,
-//     },
-//   );
-
-//   if (!updatedTodo) {
-//     return res.status(404).json({ message: "No Todo present" });
-//   }
-//   res.status(200).json(updatedTodo);
-// };
 
 const updateTodo = async (req, res, next) => {
   try {
@@ -152,17 +105,7 @@ const updateTodo = async (req, res, next) => {
 };
 
 
-// const deleteTodo = async (req, res) => {
-//   await Todo.findOneAndDelete({
-//     _id: req.params.id,
-//     owner: req.user.userId,
-//   });
 
-//   if (!todo) {
-//     return res.status(404).json({ message: "Todo not found" });
-//   }
-//   res.status(200).json({ message: "Todo deleted successfully" });
-// };
 
 const deleteTodo = async (req, res, next) => {
   try {
@@ -186,15 +129,7 @@ const deleteTodo = async (req, res, next) => {
 };
 
 
-// const toggleTodo = async (req, res) => {
-//   const todo = await Todo.findOne({
-//     _id: req.params.id,
-//     owner: req.user.userId,
-//   });
-//   todo.completed = !todo.completed;
-//   await todo.save();
-//   res.json(todo);
-// };
+
 
 const toggleTodo = async (req, res, next) => {
   try {
